64da0c3aae8ab900d37f0225632b602f98efedf8
Thai-VuDuc169 <vuducthaibka62@gmail.com>
2021-05-13 23:10:00 +0700

    splitting images

--- SplitWIDERFACEImages.py
+++ SplitWIDERFACEImages.py
@@ -9,13 +9,12 @@ os.chdir(current_folder_path)
 print("Hello {}!, the current folder path is '{}' in this system"
       .format(os.getlogin(), os.getcwd()))
 
-PATH_READ_FOLDER = current_folder_path + r"\WIDERFACE\Images"
-PATH_WRITE_FOLDER = current_folder_path + r"\WIDERFACE\Splited_Images"
+PATH_READ_FOLDER = current_folder_path + r"/WIDERFACE/Images"
+PATH_WRITE_FOLDER = current_folder_path + r"/WIDERFACE/Splited_Images"
 
 
-# for sub_folder in tqdm(os.listdir(current_folder_path + PATH_READ_FOLDER)):
-sub_folder = os.listdir(PATH_READ_FOLDER)[0]
-for file in os.listdir( PATH_READ_FOLDER + "\\" + sub_folder ):
-   shutil.copyfile( PATH_READ_FOLDER + r"\{}\{}".format(sub_folder,file) , 
-                     PATH_WRITE_FOLDER)
+for sub_folder in tqdm(os.listdir(PATH_READ_FOLDER)):
+      for file in os.listdir( PATH_READ_FOLDER + "/" + sub_folder ):
+            shutil.copyfile( PATH_READ_FOLDER + r"/{}/{}".format(sub_folder,file) 
+                        ,PATH_WRITE_FOLDER + r"/{}".format(file))
 
